2c52639b348bd5efd14dc6f512289ba9d91b8d8f
Ubuntu <ubuntu@ip-172-31-3-237.ec2.internal>
2016-04-11 01:14:20 +0000

    finished

--- Assignment1/covtype_error_output_code.py
+++ Assignment1/covtype_error_output_code.py
@@ -10,12 +10,12 @@ from joblib import Parallel, delayed
 import multiprocessing
 num_cores = multiprocessing.cpu_count()
 
-N_TRAIN = 522911
-
+N_TRAIN = 80000
+CAP = 100000
 def loadData():
     # import data
-    X = [list(map(int, x.split(',')[:-1])) for x in open('covtype.data').read().splitlines()]
-    _Y = [x.split(',')[-1] for x in open('covtype.data').read().splitlines()]
+    X = [list(map(int, x.split(',')[:-1])) for x in open('covtype.data').read().splitlines()[:CAP]]
+    _Y = [x.split(',')[-1] for x in open('covtype.data').read().splitlines()[:CAP]]
     Y = [int(x) - 1 for x in _Y]
     
     xTrain, xTest, yTrain, yTest = cv.train_test_split(np.array(X), np.array(Y), train_size = N_TRAIN/len(X), random_state = 13)
@@ -43,18 +43,18 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.multiclass import OutputCodeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import LinearSVC
-
+from sklearn.metrics import accuracy_score
 xTrain, xTest, yTrain, yTest = loadData()
 
-adaboost = AdaBoostClassifier(n_estimators = 2)
-randomforest = RandomForestClassifier(n_estimators = 256) 
+adaboost = AdaBoostClassifier(n_estimators = 128)
+randomforest = RandomForestClassifier(n_estimators = 128) 
 
 ESTIMATORS = { 
     'SVM-OVA': OutputCodeClassifier(LinearSVC(loss="squared_hinge", penalty="l2", C=1000, dual=False, tol=1e-3)),
     'BOOSTING-OVA': OutputCodeClassifier(estimator = adaboost),
     'RANDOMFOREST-OVA': OutputCodeClassifier(estimator = randomforest),
 }
-code_size = {0.5 , 0.6 , 0.7 , 0.8 , 0.9 , 1, 1.1 , 1.2 , 1.3 , 1.4 , 1.5 , 1.6 , 1.7 , 1.8}
+code_size = {0.2, 0.3, 0.4, 0.5 , 0.6 , 0.7 , 0.8 , 0.9 , 1, 1.1 , 1.2 , 1.3 , 1.4 , 1.5 , 1.6 , 1.7 , 1.8}
 
 print("Training Classifiers")
 print("====================")
